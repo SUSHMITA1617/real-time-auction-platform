ad6192b9727764f2a98816d75f30404d722b7f31
Sushmita <sushmitaraj15@gmail.com>
2026-02-24 23:48:12 +0530

    server db setup

---
+++ server/src/server.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Auction API Running");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
