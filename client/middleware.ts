f9d2ae02938b94d94055bb68be285ecd0ea7c0fc
Sushmita <sushmitaraj15@gmail.com>
2026-02-26 16:51:59 +0530

    updated

--- client/middleware.ts
+++ client/middleware.ts
@@ -13,11 +13,6 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(loginUrl);
   }
 
-  if (token && pathname === "/login") {
-    const dashboardUrl = new URL("/dashboard", request.url);
-    return NextResponse.redirect(dashboardUrl);
-  }
-
   return NextResponse.next();
 }
 
